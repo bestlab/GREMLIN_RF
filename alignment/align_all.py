113c3e71576c319a06519255e47a2e422e210323
thoppe <travis.hoppe@gmail.com>
2015-11-18 13:22:14 -0500

    Finish align statement

--- alignment/align_all.py
+++ alignment/align_all.py
@@ -4,15 +4,29 @@ F_FASTA = glob.glob("fasta/*.fasta")
 
 os.system('mkdir -p aln')
 
-bcmd = "./hhblits -i {f_fasta} -d uniprot20_2013_03/uniprot20_2013_03 -oa3m {f_aln} -n 4 -diff inf -cov 75 -e 0.0000000001 -cpu 20 -maxmem 12"
+## Run HHBlits
+
+bcmd = ("./hhblits -i {f_fasta} "
+        "-d uniprot20_2013_03/uniprot20_2013_03 "
+        "-oa3m {f_aln} "
+        "-n 4 "
+        "-diff inf "
+        "-cov 75 "
+        "-e 0.0000000001 "
+        "-cpu 20 "
+        "-maxmem 12 ")
 
 for f_fasta in F_FASTA:
     pdb = os.path.basename(f_fasta).split('.')[0]
     f_aln = "aln/{}.aln".format(pdb)
+    f_aln_zip = f_aln + '.bz2'
 
-    if os.path.exists(f_aln):
+    if os.path.exists(f_aln) or os.path.exists(f_aln_zip):
         continue
 
     cmd = bcmd.format(f_aln=f_aln,f_fasta=f_fasta)
     print "starting", f_fasta
     os.system(cmd)
+
+
+    
